dfbdef30ee9fed82c2f13197e7cc2c013336b004
cherfongfoo <fools@singnet.com.sg>
2019-02-03 23:17:45 +0800

    Record some actions of users. Added signal for an action

--- Chapter04/bookmarks/images/views.py
+++ Chapter04/bookmarks/images/views.py
@@ -10,6 +10,13 @@ from common.decorators import ajax_required
 from django.http import HttpResponse
 from django.core.paginator import Paginator, EmptyPage, \
                                   PageNotAnInteger
+from actions.utils import create_action
+from django.db.models import Count
+
+
+#This is just an example
+#images_by_popularity = Image.objects.order_by('-total_likes')
+
 
 
 @ajax_required
@@ -26,6 +33,7 @@ def image_create(request):
             # assign current user to the item
             new_item.user = request.user
             new_item.save()
+            create_action(request.user, 'bookmarked image', new_item)
             messages.success(request, 'Image added successfully')
 
             # redirect to new created item detail view
@@ -58,6 +66,7 @@ def image_like(request):
             image = Image.objects.get(id=image_id)
             if action == 'like':
                 image.users_like.add(request.user)
+                create_action(request.user, 'likes', image)
             else:
                 image.users_like.remove(request.user)
             return JsonResponse({'status':'ok'})
